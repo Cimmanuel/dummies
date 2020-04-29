bf4d69f200a2024970c2bda9796e569f9edea3aa
Immanuel Kolapo <immanuelcaspian@gmail.com>
2020-04-29 16:12:49 +0100

    Updated DATABASES setting

--- dummies/settings.py
+++ dummies/settings.py
@@ -67,10 +67,9 @@ WSGI_APPLICATION = 'dummies.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 
